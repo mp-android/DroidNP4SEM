757da08ec9f570abb815594c15c9dfa2f0f0099e
mamor <mamor.dev@gmail.com>
2012-11-08 10:17:51 +0900

    Changed android:label for MainActivity

--- src/net/madroom/dnp4sem/MainActivity.java
+++ src/net/madroom/dnp4sem/MainActivity.java
@@ -1,8 +1,7 @@
 package net.madroom.dnp4sem;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
